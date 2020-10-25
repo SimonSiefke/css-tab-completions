afad800022a0a0f1bd929a861d395c623fc89ac1
Simon Siefke <simon.siefke@gmail.com>
2020-10-25 20:53:55 +0100

    add margin 0 auto

--- src/test/test-util/generated-tests/margin.test.ts
+++ src/test/test-util/generated-tests/margin.test.ts
@@ -1,5 +1,8 @@
 import { getTabCompletion } from '../../../tabCompletion'
 
 test('margin.test.txt', () => {
-  expect(getTabCompletion('m')).toEqual('margin:')
+  expect(getTabCompletion('margin')).toEqual('margin: ')
+  expect(getTabCompletion('m')).toEqual('margin: ')
+  expect(getTabCompletion('ma')).toEqual('margin: auto;')
+  expect(getTabCompletion('m0a')).toEqual('margin: 0 auto;')
 })
