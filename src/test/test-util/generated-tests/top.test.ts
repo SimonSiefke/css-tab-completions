734e54b44e5735793a4a7e0c365e3351340f4a7e
Simon Siefke <simon.siefke@gmail.com>
2020-10-25 20:55:00 +0100

    add top 0

--- src/test/test-util/generated-tests/top.test.ts
+++ src/test/test-util/generated-tests/top.test.ts
@@ -1,5 +1,7 @@
 import { getTabCompletion } from '../../../tabCompletion'
 
 test('top.test.txt', () => {
+  expect(getTabCompletion('top')).toEqual('top: ')
   expect(getTabCompletion('t')).toEqual('top: ')
+  expect(getTabCompletion('t0')).toEqual('top: 0;')
 })
