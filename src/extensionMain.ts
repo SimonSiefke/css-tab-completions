c252414545a0c36e8fb451ea04f8c937ffda16a2
Simon Siefke <simon.siefke@gmail.com>
2020-10-25 18:59:00 +0100

    better completions

--- src/extensionMain.ts
+++ src/extensionMain.ts
@@ -4,8 +4,10 @@ import { getTabCompletion } from './tabCompletion'
 const cssTabCompletion = (textEditor: vscode.TextEditor) => {
   const wordRange = textEditor.document.getWordRangeAtPosition(
     textEditor.selection.active,
+    /[a-zA-Z\-]+/,
   )
   const partialWord = textEditor.document.getText(wordRange)
+  console.log(partialWord)
   const tabCompletion = getTabCompletion(partialWord)
   if (tabCompletion) {
     textEditor.insertSnippet(new vscode.SnippetString(tabCompletion), wordRange)
