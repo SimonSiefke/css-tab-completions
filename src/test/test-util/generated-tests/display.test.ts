c76e8dee860833f0928eec3e8c0572257497fe76
Simon Siefke <simon.siefke@gmail.com>
2020-10-25 21:01:29 +0100

    add test for display grid

--- src/test/test-util/generated-tests/display.test.ts
+++ src/test/test-util/generated-tests/display.test.ts
@@ -6,4 +6,5 @@ test('display.test.txt', () => {
   expect(getTabCompletion('db')).toEqual('display: block;')
   expect(getTabCompletion('df')).toEqual('display: flex;')
   expect(getTabCompletion('dn')).toEqual('display: none;')
+  expect(getTabCompletion('dg')).toEqual('display: grid;')
 })
